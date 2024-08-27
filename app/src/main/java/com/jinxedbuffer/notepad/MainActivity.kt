48a16fa616be863efaaba42251c392215fb296e4
jinxedbuffer <srijandhak04@gmail.com>
2024-08-27 13:04:41 +0530

    added main page

--- app/src/main/java/com/jinxedbuffer/notepad/MainActivity.kt
+++ app/src/main/java/com/jinxedbuffer/notepad/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.jinxedbuffer.notepad
 
 import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        DynamicColors.applyToActivityIfAvailable(this)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +20,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        // start cursor blink
+        val editText = findViewById<EditText>(R.id.textfield)
+        editText.requestFocus()
     }
 }
