bfef95dde415ece630101a0d4d174fba25cffd18
jinxedbuffer <srijandhak04@gmail.com>
2024-08-27 20:26:36 +0530

    added open file functionality

--- app/src/main/java/com/jinxedbuffer/notepad/MainActivity.kt
+++ app/src/main/java/com/jinxedbuffer/notepad/MainActivity.kt
@@ -1,17 +1,37 @@
 package com.jinxedbuffer.notepad
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.color.DynamicColors
 
 class MainActivity : AppCompatActivity() {
+
+    private var currentlyOpenedFileUri: Uri? = null
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        uri?.let {
+            currentlyOpenedFileUri = it
+            val text = readFromUri(it)
+            findViewById<EditText>(R.id.textfield).setText(text)
+            Toast.makeText(this, "Opened ${getFileNameFromUri(this, it)}", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DynamicColors.applyToActivityIfAvailable(this)
@@ -32,5 +52,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_open_file -> {
+                openFile()
+                true
+            }
+            R.id.action_save_file -> {
+                true
+            }
+            R.id.action_save_as -> {
+                true
+            }
+            R.id.action_settings -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun openFile() {
+        getContent.launch("*/*")
+    }
+
+    private fun readFromUri(uri: Uri?): String? {
+        uri?.let {
+            contentResolver.openInputStream(uri)?.use { inputStream ->
+                return inputStream.bufferedReader().use { it.readText() }
+            }
+        }
+        return null
+    }
+
+    @SuppressLint("Range")
+    fun getFileNameFromUri(context: Context, uri: Uri?): String? {
+        val cursor = uri?.let {
+            context.contentResolver.query(it, arrayOf(OpenableColumns.DISPLAY_NAME), null, null, null)
+        }
+        return cursor?.use {
+            it.moveToFirst()
+            it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+        }
     }
 }
